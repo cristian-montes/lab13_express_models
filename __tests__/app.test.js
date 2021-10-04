1a17777c8e31aa45349114ae8f6df6452a7dbab4
Cristian Montes <cmontesmujica@gmail.com>
2021-10-04 16:03:04 -0700

    added models, services, constructors and the post route for type

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,12 +1,27 @@
-import pool from '../lib/utils/pool.js';
-import setup from '../data/setup.js';
-import request from 'supertest';
-import app from '../lib/app.js';
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
 
 describe('demo routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
+
+  //POST AN EPISODE
+  it('stores a new type in SQL Table', async () => {
+    return request(app)
+      .post('/api/zoology/')
+      .send({ type: 'fish' })
+      .then(res => {
+        expect(res.body).toEqual({
+          id: '1',
+          type: 'fish'
+        });
+      });
+  });
+
+
 
   afterAll(() => {
     pool.end();
